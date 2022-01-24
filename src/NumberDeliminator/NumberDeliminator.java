3f74fc5642d23d347b52d8c14e7e8c0648a20e37
joneil111 <joneilnaicker111@gmail.com>
2022-01-24 17:02:47 +0200

    NumberDeliminator update str to stringbuilder

--- src/NumberDeliminator/NumberDeliminator.java
+++ src/NumberDeliminator/NumberDeliminator.java
@@ -11,19 +11,18 @@ public class NumberDeliminator implements NumberRangeSummarizer {
 
         String[] split = input.split("\s*,\s*");//this regex will split the numbers by the , regardless of a space before or after the ,
         List<String> list = Arrays.asList(split);
-        List<Integer> s =list.stream().map(Integer::parseInt).collect(Collectors.toList());
 
 //        for(int i : s){
 //            System.out.println(i);            //Display the list to check if it is correct
 //        }
 
-        return s;
+        return list.stream().map(Integer::parseInt).collect(Collectors.toList());
     }
 
     @Override
     public String summarizeCollection(Collection<Integer> input) {
 
-        String str="";
+        StringBuilder str= new StringBuilder();
         int prev =Integer.MIN_VALUE;
         int count = 0;
         for(int i :input){
@@ -36,34 +35,34 @@ public class NumberDeliminator implements NumberRangeSummarizer {
 
             if ( count > 1){
 
-                str = str+"-";
-                str = str+prev+",";
+                str.append("-");
+                str.append(prev).append(",");
             }
 
             else if(count==1){
-                str = str+","+prev+",";
+                str.append(",").append(prev).append(",");
 
             }
 
             else if(prev != Integer.MIN_VALUE) {
-                str = str + ",";
+                str.append(",");
             }
             count=0;
-            str = str +i;
+            str.append(i);
             prev = i;
 
         }
 
         if ( count > 1){
 
-            str = str+"-";
-            str = str+prev;
+            str.append("-");
+            str.append(prev);
         }
 
         else if(count==1){
-            str = str+","+prev;
+            str.append(",").append(prev);
         }
 
-        return str;
+        return str.toString();
     }
 }
